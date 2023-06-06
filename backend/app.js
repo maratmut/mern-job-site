e7d5fba45da8fc6109224e15d4adffb66c71855f
Marat Mutalipov <maratios41@gmail.com>
2023-06-06 10:13:20 +0300

    Create Error handler

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import errorHandler from './middleware/error.js';
 dotenv.config();
 
 const app = express();
@@ -17,6 +19,22 @@ mongoose
   .then(() => console.log('DB connected'))
   .catch((err) => console.log(err));
 
+//Middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '5mb' }));
+app.use(
+  bodyParser.urlencoded({
+    limit: '5mb',
+    extended: true,
+  }),
+);
+app.use(cookieParser());
+app.use(cors());
+
+//error middleware
+app.use(errorHandler);
+
+//PORT
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
